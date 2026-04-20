b532c1ee996bb02b85bb649106bd6ff23a01d695
Alex.com <bueno.zat@obmontessori.edu.ph>
2026-04-20 11:13:11 +0800

    Add cases for Fiction, Non-Fiction, and Reference sections

--- main.js
+++ main.js
@@ -2,7 +2,13 @@ function Verify() {
   var sectCode = document.getElementById('LbrStn').value;
 
   switch (sectCode)  {
-    case "F";
-      
+    case "FS";
+      document.getElementById('DLbrStn').innerhtml="You will be directed to the Fiction Section";break;
+    case "NS";
+      document.getElementById('DLbrStn').innerhtml="You will be directed to the Non-Fiction Section";break;
+    case "RS";
+      document.getElementById('DLbrStn').innerhtml="You will be directed to the Reference Section";break;
+    case "Default";
+      document.getElementById('DLbrStn').innerhtml="The section that you are looking for does not exist";break;
   }
 }
